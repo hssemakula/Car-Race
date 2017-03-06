5f7d3f6adb844be82a7762f8d453a44f99e48698
saramberr12 <higginssarah12@gmail.com>
2017-03-06 14:48:30 -0500

    Add files via upload

---
+++ CheckPoint.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * Created by sarah on 3/6/17.
+ */
+
+    public class CheckPoint implements Drawable {
+
+        int x;
+        int y;
+
+        public CheckPoint(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void draw(Graphics g)
+        {
+            g.setColor(Color.yellow);
+            g.fillOval(x-7,y-7,7,7);
+            g.setColor(Color.black);
+
+        }
+
+        public int getX(){ return x; }
+        public int getY(){ return y; }
+    }
