083d3711a490e454a95c179a13429f8b06a9b84c
kyekiriwo <h_ssemakula@post.massbay.edu>
2017-03-07 11:41:21 -0500

    Update RaceEvent.java

--- RaceEvent.java
+++ RaceEvent.java
@@ -1,6 +1,7 @@
 //Hillary Ssemakula
 import java.util.ArrayList;
 import java.util.Queue;
+import java.util.Random;
 import java.awt.Color;
 import java.awt.Graphics;
 
@@ -9,16 +10,22 @@ public class RaceEvent
 {
   private ArrayList<Car> contestants;
   private RaceTrack track;
-    
-  public RaceEvent(Color usrColor, String usrName, Integer numOpp)
+  
+  public RaceEvent(String usrName, Integer numOpp)
   {
-    innitialize(usrColor, usrName, numOpp);
+    innitialize(usrName, numOpp);
     
   } 
   
-  public void innitialize(Color usrColor, String usrName, Integer numOpp)
+  public void innitialize(String usrName, Integer numOpp)
   {
-    
+    Car userCar = new Car(generatePath(), usrName);
+    contestants = new ArrayList<Car>();
+    contestants.add(userCar);
+    for(int i = 2; i <= numOpp; i++)
+    {
+      contestants.add(new Car(generatePath(), "Racer "+i));
+    }
   } 
   
   public boolean race()
@@ -26,18 +33,32 @@ public class RaceEvent
     return false;
   }
   
-  public Queue<String> generatePath()
+  public String generatePath()
   {
-    return null;
+    String path = "";
+    ArrayList<String> allChecks = new ArrayList<String>();
+    allChecks.add("A");
+    allChecks.add("B");
+    allChecks.add("C");
+    allChecks.add("D");
+    while(allChecks.size() != 0)
+    {
+      int index = (new Random()).nextInt(allChecks.size());
+      path += allChecks.remove(index);  
+    }  
+    return path;
   }
   
   public String getContestants()
   {
-    return "";
+    String view = "";
+    for(Car c: contestants) view += c.toString() + "\n";
+    return view;
   }
   
   public void draw(Graphics g)
   {
-    
+    track.draw(g);
+    for(Car c: contestants) c.draw(g);
   }  
 }  
